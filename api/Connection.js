f7f620871546d689911426cc6674e1688f325b95
AqsalYS <aqsalyazids@gmail.com>
2026-05-30 18:56:22 +0700

    update api

--- api/Connection.js
+++ api/Connection.js
@@ -1,11 +1,5 @@
 const pgp = require('pg-promise')()
 
-const db = pgp({
-  host: process.env.POSTGRES_HOST,
-  port: 5432,
-  database: process.env.POSTGRES_DATABASE,
-  user: process.env.POSTGRES_USER,
-  password: process.env.POSTGRES_PASSWORD,
-})
+const db = pgp(process.env.NEXT_PUBLIC_SUPABASE_URL)
 
 module.exports = db
