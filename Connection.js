8dd0785b2b6d0298965c18ba42aca16191e7a091
AqsalYS <aqsalyazids@gmail.com>
2026-07-07 20:56:31 +0700

    add local debug

---
+++ Connection.js
@@ -0,0 +1,12 @@
+require("dotenv").config();
+
+const pgp = require('pg-promise')()
+
+const db = pgp({
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false },
+  max: 1,              // IMPORTANT for Vercel
+  idleTimeoutMillis: 30000
+})
+
+module.exports = db
